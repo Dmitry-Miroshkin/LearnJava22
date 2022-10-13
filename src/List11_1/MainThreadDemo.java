43c15bf7fbf59d54b215aeb9e188b2fe4c76f664
Dmitry Miroshkin <dmitry.miroshkin@gmail.com>
2022-10-14 00:04:22 +0300

    11_1 выполнено

---
+++ src/List11_1/MainThreadDemo.java
@@ -0,0 +1,20 @@
+package List11_1;
+
+public class MainThreadDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t;
+        long time = 2500;
+        t=Thread.currentThread();
+
+        System.out.println(t);
+
+        t.setName("This is MAIN thread!");
+
+        int p = t.getPriority();
+        t.setPriority(++p);
+        System.out.println(t);
+        System.out.println("Выполнение потока приостановлено на " + time/1000 + " секунд");
+        Thread.sleep(time);
+        System.out.println("Program is done!");
+    }
+}
